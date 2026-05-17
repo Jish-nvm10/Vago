cfddc8730ad32d5e06011f5b06641ac537457487
z4spectrum <z4spectrum2901@gmail.com>
2026-05-17 23:35:03 +0530

    Added root API route

--- server/server.js
+++ server/server.js
@@ -1,18 +1,45 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config({ path: __dirname + "/.env" });
+require("dotenv").config();
 
-const authRoutes = require("./routes/auth");
-const plannerRoutes = require("./routes/planner");
+const authRoutes =
+  require("./routes/auth");
+
+const plannerRoutes =
+  require("./routes/planner");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/planner", plannerRoutes);
+app.use(
+  "/api/auth",
+  authRoutes
+);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Vago Server running on port ${process.env.PORT}`);
+app.use(
+  "/api/planner",
+  plannerRoutes
+);
+
+
+/* ROOT ROUTE */
+
+app.get("/", (req, res) => {
+
+  res.send("Vago API Running 🚀");
+
+});
+
+
+const PORT =
+  process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+
+  console.log(
+    `Server running on ${PORT}`
+  );
+
 });
