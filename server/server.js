3e20216fc28408d25bec7f24108b352335e5ff59
z4spectrum <z4spectrum2901@gmail.com>
2026-05-17 23:12:25 +0530

    Restored backend folders

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config({ path: __dirname + "/.env" });
 
-const authRoutes = require("./auth");
+const authRoutes = require("./routes/auth");
 const plannerRoutes = require("./routes/planner");
 
 const app = express();
